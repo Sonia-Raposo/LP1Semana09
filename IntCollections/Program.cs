0dbf7a535e5797927faf92a9f5db7b0400c9c2bd
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-05-10 13:44:32 +0100

    Created collections

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace IntCollections
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        // Create the collections
+        List<int> list = new List<int> { 1, 10, -30, 10, -5 };
+        Stack<int> stack = new Stack<int>(new[] { 1, 10, -30, 10, -5 });
+        Queue<int> queue = new Queue<int>(new[] { 1, 10, -30, 10, -5 });
+        HashSet<int> hashSet = new HashSet<int> { 1, 10, -30, 10, -5 };
+
+        // Print each collection using a separate foreach loop
+        PrintList(list);
+        PrintStack(stack);
+        PrintQueue(queue);
+        PrintHashSet(hashSet);
     }
+
 }
